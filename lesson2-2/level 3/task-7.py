c3078efc11e01086e5340f97c6413f742c245ef2
IuriiPidvirnyi <iurii.pidvirnyi@gmail.com>
2016-05-11 19:57:28 +0300

    Added task for Level 3

---
+++ lesson2-2/level 3/task-7.py
@@ -0,0 +1,11 @@
+# Write a function to find out if the word is a palindrome.
+
+def palindrome(a):
+    if a[::1]==a[::-1]:
+        print (a + " - is a palindrome")
+    else:
+        print (a + " - is not a palindrome")
+
+if __name__ == "__main__":
+    a="abuttuba"
+palindrome(a)
